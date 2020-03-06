544cf9da930254c0de0b1a26394adfdebba39bb0
AdiGarmendia <dejima@gmail.com>
2020-03-06 10:37:51 -0600

    updated

--- heist/Program.cs
+++ heist/Program.cs
@@ -4,28 +4,78 @@ using System.Collections.Generic;
 namespace heist {
     class Program {
         static void Main (string[] args) {
-            Console.WriteLine ("Plan Your Heist!");
 
-            Dictionary<string, string> teamMember = new Dictionary<string, string> ();
+            List<Dictionary<string, string>> AllTeamInformation = new List<Dictionary<string, string>> ();
 
-            Console.Write ("What is the member's name? ");
-            string name = Console.ReadLine ();
+            int totalSkillLevel = 0;
 
-            Console.Write ("What is the member's skill level? ");
-            string skill = Console.ReadLine ();
+            int successCount = 0;
+            int failCount = 0;
 
-            Console.Write ("What is the member's courage factor? ");
-            string courage = Console.ReadLine ();
+            Console.WriteLine ("Enter your bank difficulty level");
+            int bankDifficultyLevel = int.Parse (Console.ReadLine ());
 
-            teamMember.Add ("Name", name);
-            teamMember.Add ("Skill Level", skill);
-            teamMember.Add ("Courage", courage);
+            Console.WriteLine ("Plan your heist");
+            while (true) {
 
-            Console.Clear ();
-            foreach (KeyValuePair<string, string> attribute in teamMember)
-                (
-                    Console.WriteLine ($"{attribute.Key}: {attribute.Value}")
-                )
+                Dictionary<string, string> teamMemberInfo = new Dictionary<string, string> ();
+
+                Console.WriteLine ("Enter a team member's name:");
+                string memberName = Console.ReadLine ();
+                teamMemberInfo.Add ("Member Name", memberName);
+
+                if (memberName != "") {
+                    Console.WriteLine ("Enter a team member's skill level:");
+                    teamMemberInfo.Add ("Skill Level", Console.ReadLine ());
+
+                    Console.WriteLine ("Enter a team member's courage factor:");
+                    teamMemberInfo.Add ("Courage Level", Console.ReadLine ());
+
+                    AllTeamInformation.Add (teamMemberInfo);
+
+                } else {
+                    break;
+                }
+            }
+
+            Console.WriteLine ("Enter the number of trial runs");
+            int trialRuns = int.Parse (Console.ReadLine ());
+
+            foreach (Dictionary<string, string> teamMember in AllTeamInformation)
+                foreach (KeyValuePair<string, string> item in teamMember) {
+                    // Console.WriteLine ($"{item.Key}: {item.Value}");
+
+                    if (item.Key == "Skill Level") {
+                        totalSkillLevel += (int.Parse (item.Value));
+                    }
+
+                }
+
+            for (int i = 0; i < trialRuns; i++) {
+
+                Random rand = new Random ();
+                int luckValue = rand.Next (1, 11);
+
+                int newBankDifficultyLevel = luckValue + bankDifficultyLevel;
+
+                if (totalSkillLevel >= newBankDifficultyLevel) {
+                    successCount++;
+                    Console.WriteLine ("You are going to succeed!");
+                    Console.WriteLine ($"The team's combined skill level is {totalSkillLevel}");
+                    Console.WriteLine ($"You  have {AllTeamInformation.Count} on your team");
+                    Console.WriteLine ($"The bank's diffulty level is: {newBankDifficultyLevel}");
+                } else {
+                    failCount++;
+                    Console.WriteLine ("Your mission is GOING to FAIL!");
+                    Console.WriteLine ($"The team's combined skill level is {totalSkillLevel}");
+                    Console.WriteLine ($"You  have {AllTeamInformation.Count} on your team");
+                    Console.WriteLine ($"The bank's diffulty level is: {newBankDifficultyLevel}");
+
+                }
+            }
+
+            Console.WriteLine ($"Number of succesful runs {successCount}");
+            Console.WriteLine ($"Number of failed runs {failCount}");
 
         }
     }
